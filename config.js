70c16ec53b795b82632f5fb11e3bf1d38e4af2d4
Jame3756 <75145664+Jame3756@users.noreply.github.com>
2021-09-11 16:26:45 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC3d5BB1N6sMdWrbDq_REja8ZI9RxwrT0k",
+    authDomain: "onion-f7d72.firebaseapp.com",
+    projectId: "onion-f7d72",
+    storageBucket: "onion-f7d72.appspot.com",
+    messagingSenderId: "385997643300",
+    appId: "1:385997643300:web:d39136e1040bd5d78d82b1"
+  };
